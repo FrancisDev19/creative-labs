8e6a932a137a9ee6606687ec104aa79f40b46949
Francissmiguell <xfrancismiguelx@gmail.com>
2018-10-15 17:13:01 -0400

    Footer complete

---
+++ js/background-color.js
@@ -0,0 +1,14 @@
+$(".facebook").hover(function () {
+    $("#social-section").toggleClass("color-facebook")
+    $("#social-section2").toggleClass("color-facebook")
+});
+
+$(".google-plus").hover(function () {
+    $("#social-section").toggleClass("color-googleplus")
+    $("#social-section2").toggleClass("color-googleplus")
+});
+
+$(".instagram").hover(function () {
+    $("#social-section").toggleClass("color-instagram")
+    $("#social-section2").toggleClass("color-instagram")
+});
